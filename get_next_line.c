7e902d2ee60c6cf0427a83174f50e6860cedf22c
Kaman <khairell-khazin-bin.kaman@hp.com>
2025-12-03 18:00:14 +0800

    adjust a bit

--- get_next_line.c
+++ get_next_line.c
@@ -12,18 +12,31 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*read_file(int fd)
 {
-	ssize_t	bytes_read;
+	ssize_t	byte_read;
 	char	*buffer;
+	static int	count;
 
+	count = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!buffer)
 		return (NULL);
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
+	if (byte_read <= 0)
+	{
+		free(buffer);
 		return (NULL);
+	}
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*buffer;
+
+	buffer = read_file(fd);
 	return (buffer);
 }
